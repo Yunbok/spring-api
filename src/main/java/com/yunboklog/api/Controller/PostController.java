96905908f013de73be706394353be42a980d18a6
Yunbok <qodbsqhr@naver.com>
2022-08-17 23:59:28 +0900

    test: post 요청 테스트 추가

--- src/main/java/com/yunboklog/api/Controller/PostController.java
+++ src/main/java/com/yunboklog/api/Controller/PostController.java
@@ -1,14 +1,18 @@
 package com.yunboklog.api.Controller;
 
+import com.yunboklog.api.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class PostController {
 
-    @GetMapping("/posts")
-    public String get() {
+    @PostMapping("/posts")
+    public String get(@RequestBody PostCreate param) {
+
+        log.info("param = {}", param.toString());
         return "Hello World";
     }
 }
