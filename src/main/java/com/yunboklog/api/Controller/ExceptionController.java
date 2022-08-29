3ffad39b7c8baa8a7f7ab904a1a0e8bc3ced9517
Yunbok <qodbsqhr@naver.com>
2022-08-29 23:22:18 +0900

    feat: /posts 테스트 코드 추가 및 익셉션 핸들러 작동 테스트

--- src/main/java/com/yunboklog/api/Controller/ExceptionController.java
+++ src/main/java/com/yunboklog/api/Controller/ExceptionController.java
@@ -1,14 +1,12 @@
 package com.yunboklog.api.Controller;
 
 
+import com.yunboklog.api.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,15 +17,16 @@ public class ExceptionController {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> invalidRequestHandler(MethodArgumentNotValidException e) {
-        FieldError fieldError = e.getFieldError();
-        String field = fieldError.getField();
-        String message = fieldError.getDefaultMessage();
+    @ResponseBody
+    public ErrorResponse invalidRequestHandler(MethodArgumentNotValidException e) {
+//            FieldError fieldError = e.getFieldError();
+//            String field = fieldError.getField();
+//            String message = fieldError.getDefaultMessage();
+        ErrorResponse response = new ErrorResponse("400", "잘못된 요청입니다.");
 
-        Map<String, String> response = new HashMap<>();
-        response.put(field, message);
-        log.info("exceptionHandler error", e);
-
+        for (FieldError fieldError : e.getFieldErrors()) {
+            response.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
+        }
         return response;
     }
 }
--- src/test/java/com/yunboklog/api/Controller/PostControllerTest.java
+++ src/test/java/com/yunboklog/api/Controller/PostControllerTest.java
@@ -8,10 +8,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @WebMvcTest
 class PostControllerTest {
@@ -20,16 +21,17 @@ class PostControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("/posts 요청시 Hello World 를 출력")
+    @DisplayName("/posts 요청 성공시 빈 Json 출력{}")
     void test() throws Exception {
 
         mockMvc.perform(MockMvcRequestBuilders.post("/posts")
                         .contentType(MediaType.APPLICATION_JSON)
+                        //.content("{\"title\": \"제목입니다.\"}")
                         .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다. \"}")
 
                 )
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("Hello World"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("{}"))
                 .andDo(MockMvcResultHandlers.print());
 
     }
@@ -40,11 +42,28 @@ class PostControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders.post("/posts")
                         .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"content\": \"내용입니다. \"}")
+
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    @DisplayName("/posts 요청 성공")
+    void test3() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다. \"}")
 
                 )
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요."))
+                .andExpect(status().isOk())
+                .andExpect(content().string("{}"))
                 .andDo(MockMvcResultHandlers.print());
 
     }
