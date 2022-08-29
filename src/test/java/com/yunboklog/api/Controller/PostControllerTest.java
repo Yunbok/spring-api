bd90e55fc8269b6221dd57120c2933d3e3cfef58
Yunbok <qodbsqhr@naver.com>
2022-08-29 23:52:56 +0900

    feat: 게시글 저장 API 구현

--- src/test/java/com/yunboklog/api/Controller/PostControllerTest.java
+++ src/test/java/com/yunboklog/api/Controller/PostControllerTest.java
@@ -1,24 +1,37 @@
 package com.yunboklog.api.Controller;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.yunboklog.api.domain.Post;
+import com.yunboklog.api.repository.PostRepository;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@WebMvcTest
+@AutoConfigureMockMvc
+@SpringBootTest
 class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @BeforeEach
+    void clean() {
+        postRepository.deleteAll();
+
+    }
+
 
     @Test
     @DisplayName("/posts 요청 성공시 빈 Json 출력{}")
@@ -54,17 +67,21 @@ class PostControllerTest {
     }
 
     @Test
-    @DisplayName("/posts 요청 성공")
+    @DisplayName("/posts 요청시 DB에 값이 저장된다.")
     void test3() throws Exception {
-
+        //when
         mockMvc.perform(MockMvcRequestBuilders.post("/posts")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다. \"}")
 
                 )
                 .andExpect(status().isOk())
-                .andExpect(content().string("{}"))
                 .andDo(MockMvcResultHandlers.print());
+        //then
+        assertEquals(1L, postRepository.count());
 
+        Post post = postRepository.findAll().get(0);
+        assertEquals("제목입니다.", post.getTitle());
+        assertEquals("내용입니다.", post.getContent());
     }
 }
